77148f9f82abdb97b772e4a0da83c3e0a5ea16f2
codesfromannywhere <codesfromannywhere@gmail.com>
2023-06-16 10:02:09 +0200

    added data

--- back-end/scripts/seed.js
+++ back-end/scripts/seed.js
@@ -1,3 +1,9 @@
 import mongoose from "mongoose";
 
 import { PostModel, UserModel } from "../model/index.js";
+
+import data from "../data/seed_data.json" assert {type:"json"}; 
+
+const authors = data.blogPosts.map((article) => article.author); 
+console.log(authors);
+
